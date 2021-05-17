2483c779a24d7a179d3624cef9c277253b98ab9e
lisanne <lisannenederpelt@hotmail.com>
2021-05-17 20:48:45 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -4,16 +4,23 @@
 var rotationSpeed = -0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
+function spinXas(){
+	myOtherBox.object3D.rotation.x += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+}
+
+	setInterval(spinXas, 16); //equivalent to 60 fps
+
+	function spinYas(){
+		myOtherBox.object3D.rotation.y += rotationSpeed;
+		console.log(myOtherBox.object3D.rotation.y);
+	}
+
+		setInterval(spinYas, 16); //equivalent to 60 fps
+
 function spinZas(){
 	myOtherBox.object3D.rotation.z += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.z);
 }
 
 	setInterval(spinZas, 16); //equivalent to 60 fps
-
-function spinYas(){
-	myOtherBox.object3D.rotation.y += rotationSpeed;
-	console.log(myOtherBox.object3D.rotation.y);
-}
-
-	setInterval(spinYas, 16); //equivalent to 60 fps
